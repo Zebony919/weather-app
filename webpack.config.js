4d1b0ea76c3e6b645523ff6cac030fa915e72e3d
Xavier Miller <Zebony9@gmail.com>
2026-01-29 18:14:09 +1300

    small ui change

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,21 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); 
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/weather-app/",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Add this
+    },
+    port: 8080,
+    open: true,
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
